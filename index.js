e8135fd532fbdfa68ed712239cb1e30ad9e3b2b7
reyad9009 <tarekgithub09@gmail.com>
2025-01-04 06:41:10 +0600

    successfuly handle purchase item data

--- index.js
+++ index.js
@@ -57,28 +57,52 @@ async function run() {
       res.send(result);
     })
 
-    // when user click purchase button then store purchase details in db
-    app.post('/foods/purchase', async (req, res) => {
-      const newFood = req.body;
-      const result = await foodPurchase.insertOne(newFood);
-      res.send(result);
-    })
-    // update data by every purchase
-    app.patch("/food/purchase/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updatedQuantity = req.body;
-      const update = { $set: { quantity: updatedQuantity.quantity } };
-      const result = await foodPurchase.updateOne(filter, update);
-      res.send(result);
+    
+    // when user click purchase button then store purchase details and also update quantity in db
+    app.post("/foods/purchase", async (req, res) => {
+      const { foodId, quantity } = req.body;
+      try {
+        // Check if the food item already exists
+        const existingFood = await foodPurchase.findOne({ foodId });
+        if (existingFood) {
+          // Update only the quantity
+          const updateResult = await foodPurchase.updateOne(
+            { foodId },
+            { $inc: { quantity: parseInt(quantity, 10) } }
+          );
+          res.status(200).json({
+            updatedQuantity: existingFood.quantity + parseInt(quantity, 10),
+          });
+        }
+        else {
+          const newFood = req.body;
+          const result = await foodPurchase.insertOne(newFood);
+          res.send(result);
+
+        }
+
+      } catch (error) {
+        console.error("Error handling food purchase:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
     });
 
-    app.get('/foods/purchase/:foodId', async (req, res) => {
-      const foodId = req.params.foodId;
-      const query = { foodId: foodId }
-      const result = await foodPurchase.findOne(query);
-      res.send(result);
-    });
+    // update data by every purchase
+    // app.patch("/food/purchase/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id) };
+    //   const updatedQuantity = req.body;
+    //   const update = { $inc: { quantity: updatedQuantity.quantity } };
+    //   const result = await foodPurchase.updateOne(filter, update);
+    //   res.send(result);
+    // });
+
+    // app.get('/foods/purchase/:foodId', async (req, res) => {
+    //   const foodId = req.params.foodId;
+    //   const query = { foodId: foodId }
+    //   const result = await foodPurchase.findOne(query);
+    //   res.send(result);
+    // });
 
     // when user click purchase button then update quantity
     app.patch("/food/:id", async (req, res) => {
@@ -112,40 +136,40 @@ app.listen(port, () => {
 
 
 
-    // http://localhost:5000/foods/purchase/:foodId
-    // http://localhost:5000/foods/details/:id
+// http://localhost:5000/foods/purchase/:foodId
+// http://localhost:5000/foods/details/:id
 
 
-    // {
-    //   "_id": "67781927a46602b1b1c99926",
-    //   "foodName": "Chicken Biryani",
-    //   "price": "10",
-    //   "quantity": "40",
-    //   "name": "",
-    //   "email": "",
-    //   "date": "01/03/2025",
-    //   "foodId": "67746a62f9cd0f3b095a8d36",
-    //   "image": "https://i.ibb.co.com/6WMrZgz/Chicken-Biryani.png"
-    // }
-    // {
-    //   "_id": "67781927a46602b1b1c99927",
-    //   "foodName": "Chicken Biryani",
-    //   "price": "10",
-    //   "quantity": "40",
-    //   "name": "",
-    //   "email": "",
-    //   "date": "01/03/2025",
-    //   "foodId": "67746a62f9cd0f3b095a8d38",
-    //   "image": "https://i.ibb.co.com/6WMrZgz/Chicken-Biryani.png"
-    // }
-    // {
-    //   "_id": "67781927a46602b1b1c99927",
-    //   "foodName": "Chicken Biryani",
-    //   "price": "10",
-    //   "quantity": "40",
-    //   "name": "",
-    //   "email": "",
-    //   "date": "01/03/2025",
-    //   "foodId": "67746a62f9cd0f3b095a8d36",
-    //   "image": "https://i.ibb.co.com/6WMrZgz/Chicken-Biryani.png"
-    // }
+// {
+//   "_id": "67781927a46602b1b1c99926",
+//   "foodName": "Chicken Biryani",
+//   "price": "10",
+//   "quantity": "40",
+//   "name": "",
+//   "email": "",
+//   "date": "01/03/2025",
+//   "foodId": "67746a62f9cd0f3b095a8d36",
+//   "image": "https://i.ibb.co.com/6WMrZgz/Chicken-Biryani.png"
+// }
+// {
+//   "_id": "67781927a46602b1b1c99927",
+//   "foodName": "Chicken Biryani",
+//   "price": "10",
+//   "quantity": "40",
+//   "name": "",
+//   "email": "",
+//   "date": "01/03/2025",
+//   "foodId": "67746a62f9cd0f3b095a8d38",
+//   "image": "https://i.ibb.co.com/6WMrZgz/Chicken-Biryani.png"
+// }
+// {
+//   "_id": "67781927a46602b1b1c99927",
+//   "foodName": "Chicken Biryani",
+//   "price": "10",
+//   "quantity": "40",
+//   "name": "",
+//   "email": "",
+//   "date": "01/03/2025",
+//   "foodId": "67746a62f9cd0f3b095a8d36",
+//   "image": "https://i.ibb.co.com/6WMrZgz/Chicken-Biryani.png"
+// }
