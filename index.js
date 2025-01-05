78502300fe7902aaf9d638de71466bae8e7a22dc
reyad9009 <tarekgithub09@gmail.com>
2025-01-05 22:24:35 +0600

    add delete operation for delete ordered food

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ async function run() {
       const result = await userFoodPurchase.insertOne(newFood);
       res.send(result);
     })
-    
+
     // Get orders data by logged-in user's email
     app.get('/my-orders/:email', async (req, res) => {
       const email = req.params.email;
@@ -160,6 +160,13 @@ async function run() {
       res.send(result)
     });
 
+    //logged use
+    app.delete('/my-orders/delete/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userFoodPurchase.deleteOne(query);
+      res.send(result);
+    })
 
 
   } finally {
